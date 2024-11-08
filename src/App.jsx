54027dc6086907292367456bfca4e937334e7c91
LittleMistake <svillarroel@insjoaquimmir.cat>
2024-11-08 12:17:32 +0100

    plugin text y fadeout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
 import gsap from 'gsap';
 import {useGSAP} from '@gsap/react';
 import './App.css'
-import {ScrollTrigger} from "gsap/ScrollTrigger";
+import { TextPlugin } from "gsap/TextPlugin";
 
 function App() {
     useGSAP(() => {
-            gsap.registerPlugin(ScrollTrigger);
-            gsap.to(".c", {
-                scrollTrigger: {
-                    trigger: ".c",
-                    start: "center center",
-                    end: "center 20%",
-                    markers: true,
-                    toggleActions: "restart pause reverse pause"
-                },
-                x: 400,
-                rotation: 360,
-                duration: 3
-            });
+            gsap.registerPlugin(TextPlugin)
+            gsap.defaults({ease: "none"});
+            const animacion = gsap.timeline({ repeatDelay: 1});
+            if (animacion.isActive()){return;}
+            const animacionFadeout = animacion.to("h1 span", {text: " con más texto de prueba", duration: 2, opacity: 1})
+            animacionFadeout.to("h1 span", {opacity:0})
         }
     );
     return (
         <div className="App">
-            <div className="box a">a</div>
-            <div className={"espacio"}></div>
-            <div className="box b">b</div>
-            <div className={"espacio"}></div>
-            <div className="box c">c</div>
-            <div className={"espacio"}></div>
-            <div className={"espacio"}></div>
-            <div className={"espacio"}></div>
+            <div className="wrapper">
+                <h1>Texto de prueba, <span></span></h1>
+            </div>
         </div>
     )
 }
